b4029c66e8c8688b2a8bd8ad9a7e62a9603fd1ec
Aqsa Naseer <163171532+aqsanaseer11@users.noreply.github.com>
2024-03-28 22:22:12 +0500

    line changing

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
-// console.log(randomNumber)
+console.log("Wellcome to number guessing game");
 const answer = await inquirer.prompt([{
         name: "userGuessedNumber",
         type: "number",
         message: "Please guess a number between 1 - 6 :"
     }]);
-console.log("Wellcome to number guessing game");
 if (answer.userGuessedNumber === randomNumber) {
     console.log("congratulation! you guessed right number");
 }
